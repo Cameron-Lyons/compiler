85bf2555d73b6624f8cfb060c6a1dea023555fa6
Cameron Lyons <cameron.lyons2@gmail.com>
2025-01-07 17:28:12 -0600

    opconstant

--- src/vm.rs
+++ src/vm.rs
@@ -1,4 +1,4 @@
-use crate::code::Instructions;
+use crate::code::{Instructions, OPCONSTANT};
 use crate::compiler::Bytecode;
 use crate::object::Object;
 
@@ -28,13 +28,43 @@ impl VM {
         self.stack.get(self.sp - 1).and_then(|v| v.as_ref())
     }
 
-    pub fn run(&self) -> Result<(), String> {
+    pub fn run(&mut self) -> Result<(), String> {
         let mut ip = 0;
         while ip < self.instructions.len() {
             let opcode = self.instructions.0[ip];
             ip += 1;
-            // match opcode {}
+            match opcode {
+                OPCONSTANT => {
+                    let const_index = Self::read_uint16(&self.instructions.0[ip..]);
+                    ip += 2;
+
+                    let constant = self
+                        .constants
+                        .get(const_index as usize)
+                        .ok_or_else(|| format!("Constant not found at index {}", const_index))?;
+
+                    self.push(constant.clone());
+                }
+                _ => return Err(format!("Unknown opcode: {}", opcode)),
+            }
         }
+        Ok(())
+    }
+
+    fn read_uint16(bytes: &[u8]) -> u16 {
+        ((bytes[0] as u16) << 8) | (bytes[1] as u16)
+    }
+
+    fn push(&mut self, obj: Object) -> Result<(), String> {
+        if self.sp >= STACK_SIZE {
+            return Err("Stack Overflow".to_string());
+        }
+        if self.stack.len() <= self.sp {
+            self.stack.push(Some(obj));
+        } else {
+            self.stack[self.sp] = Some(obj);
+        }
+        self.sp += 1;
         Ok(())
     }
 }
