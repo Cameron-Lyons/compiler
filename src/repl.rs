dc9a6027b86099ce730b0c300e3d04270034e2c9
Cameron Lyons <cameron.lyons2@gmail.com>
2025-01-13 19:27:25 -0600

    repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,49 +1,72 @@
-use std::io::{BufRead, Write};
+use std::io::{self, BufRead, Write};
 
-use crate::compiler;
-use crate::lexer;
-use crate::parser;
-use crate::vm;
+use crate::compiler::{Compiler, SymbolTable};
+use crate::lexer::Lexer;
+use crate::object::{Object, BUILTINS};
+use crate::parser::Parser;
+use crate::vm::{GLOBAL_SIZE, VM};
 
-const PROMPT: &str = ">> ";
+pub const PROMPT: &str = ">> ";
 
-pub fn start<R: BufRead, W: Write>(input: R, mut output: W) {
-    let mut scanner = input.lines();
+pub fn start<R: BufRead, W: Write>(input: &mut R, output: &mut W) {
+    let mut constants: Vec<Object> = vec![];
+    let mut globals: Vec<Object> = vec![Object::Null(crate::object::Null); GLOBAL_SIZE];
+    let mut symbol_table = SymbolTable::new();
 
-    while let Some(Ok(line)) = scanner.next() {
+    for (i, builtin_def) in BUILTINS.iter().enumerate() {
+        symbol_table.define_builtin(i, &builtin_def.name);
+    }
+
+    loop {
         write!(output, "{}", PROMPT).unwrap();
+        output.flush().unwrap();
 
-        let l = lexer::Lexer::new(&line);
-        let mut p = parser::Parser::new(l);
+        let mut line = String::new();
+        match input.read_line(&mut line) {
+            Ok(0) => {
+                return;
+            }
+            Ok(_) => {
+                let mut lexer = Lexer::new(&line);
+                let mut parser = Parser::new(&mut lexer);
+                let program = parser.parse_program();
 
-        let program = p.parse_program();
-        if !p.errors().is_empty() {
-            print_parser_errors(&mut output, &p.errors());
-            continue;
-        }
+                let errors = parser.errors();
+                if !errors.is_empty() {
+                    print_parse_errors(output, errors);
+                    continue;
+                }
 
-        let mut comp = compiler::Compiler::new();
-        if let Err(err) = comp.compile(&program) {
-            writeln!(output, "Woops! Compilation failed:\n{}", err).unwrap();
-            continue;
-        }
+                let mut compiler = Compiler::new_with_state(symbol_table, constants);
+                if let Err(e) = compiler.compile(&program) {
+                    writeln!(output, "Woops! Compilation failed:\n {}", e).unwrap();
+                    continue;
+                }
 
-        let mut machine = vm::VM::new(comp.bytecode());
-        if let Err(err) = machine.run() {
-            writeln!(output, "Woops! Executing bytecode failed:\n{}", err).unwrap();
-            continue;
-        }
+                let bytecode = compiler.bytecode();
+                constants = bytecode.constants.clone();
+                symbol_table = compiler.symbol_table();
 
-        if let Some(stack_top) = machine.stack_top() {
-            writeln!(output, "{}", stack_top.inspect()).unwrap();
-        } else {
-            writeln!(output, "Stack is empty").unwrap();
+                let mut vm = VM::new_with_globals_store(bytecode, globals.clone());
+                if let Err(e) = vm.run() {
+                    writeln!(output, "Woops! Executing bytecode failed:\n {}", e).unwrap();
+                    continue;
+                }
+
+                let last_popped = vm.last_popped_stack_elem();
+                write!(output, "{}\n", last_popped.inspect()).unwrap();
+
+                globals = vm.globals;
+            }
+            Err(_) => {
+                return;
+            }
         }
     }
 }
 
-fn print_parser_errors<W: Write>(output: &mut W, errors: &[String]) {
-    for error in errors {
-        writeln!(output, "Parser error: {}", error).unwrap();
+fn print_parse_errors<W: Write>(output: &mut W, errors: &[String]) {
+    for msg in errors {
+        writeln!(output, "\t{}", msg).unwrap();
     }
 }
