2ae25f4a627bc8147475ac340989b6abf6abe391
Cameron Lyons <cameron.lyons2@gmail.com>
2025-01-28 14:18:51 -0600

    refactor compiler

--- compiler/compiler.rs
+++ compiler/compiler.rs
@@ -15,6 +15,22 @@ struct CompilationScope {
     previous_instruction: EmittedInstruction,
 }
 
+impl Default for CompilationScope {
+    fn default() -> Self {
+        Self {
+            instructions: Instructions { data: vec![] },
+            last_instruction: EmittedInstruction {
+                opcode: OpNull,
+                position: 0,
+            },
+            previous_instruction: EmittedInstruction {
+                opcode: OpNull,
+                position: 0,
+            },
+        }
+    }
+}
+
 pub struct Compiler {
     pub constants: Vec<Rc<Object>>,
     pub symbol_table: SymbolTable,
@@ -36,37 +52,29 @@ pub struct EmittedInstruction {
 type CompileError = String;
 
 impl Compiler {
-    pub fn new() -> Compiler {
-        let main_scope = CompilationScope {
-            instructions: Instructions { data: vec![] },
-            last_instruction: EmittedInstruction {
-                opcode: OpNull,
-                position: 0,
-            },
-            previous_instruction: EmittedInstruction {
-                opcode: OpNull,
-                position: 0,
-            },
-        };
+    const PLACEHOLDER_ADDRESS: usize = 0;
+
+    pub fn new() -> Self {
+        let main_scope = CompilationScope::default();
 
         let mut symbol_table = SymbolTable::new();
         for (key, value) in BuiltIns.iter().enumerate() {
             symbol_table.define_builtin(key, value.0.to_string());
         }
 
-        return Compiler {
+        Compiler {
             constants: vec![],
             symbol_table,
             scopes: vec![main_scope],
             scope_index: 0,
-        };
+        }
     }
 
-    pub fn new_with_state(symbol_table: SymbolTable, constants: Vec<Rc<Object>>) -> Compiler {
-        let mut compiler = Compiler::new();
+    pub fn new_with_state(symbol_table: SymbolTable, constants: Vec<Rc<Object>>) -> Self {
+        let mut compiler = Self::new();
         compiler.constants = constants;
         compiler.symbol_table = symbol_table;
-        return compiler;
+        compiler
     }
 
     pub fn compile(&mut self, node: &Node) -> Result<Bytecode, CompileError> {
@@ -84,7 +92,7 @@ impl Compiler {
             }
         }
 
-        return Ok(self.bytecode());
+        Ok(self.bytecode())
     }
 
     fn compile_stmt(&mut self, s: &Statement) -> Result<(), CompileError> {
@@ -95,21 +103,21 @@ impl Compiler {
                     .define(let_statement.identifier.kind.to_string());
                 self.compile_expr(&let_statement.expr)?;
                 if symbol.scope == SymbolScope::Global {
-                    self.emit(Opcode::OpSetGlobal, &vec![symbol.index]);
+                    self.emit(Opcode::OpSetGlobal, &[symbol.index]);
                 } else {
-                    self.emit(Opcode::OpSetLocal, &vec![symbol.index]);
+                    self.emit(Opcode::OpSetLocal, &[symbol.index]);
                 }
-                return Ok(());
+                Ok(())
             }
             Statement::Return(r) => {
                 self.compile_expr(&r.argument)?;
-                self.emit(Opcode::OpReturnValue, &vec![]);
-                return Ok(());
+                self.emit(Opcode::OpReturnValue, &[]);
+                Ok(())
             }
             Statement::Expr(e) => {
                 self.compile_expr(e)?;
-                self.emit(OpPop, &vec![]);
-                return Ok(());
+                self.emit(OpPop, &[]);
+                Ok(())
             }
         }
     }
@@ -135,9 +143,9 @@ impl Compiler {
                 }
                 Literal::Boolean(i) => {
                     if i.raw {
-                        self.emit(OpTrue, &vec![]);
+                        self.emit(OpTrue, &[]);
                     } else {
-                        self.emit(OpFalse, &vec![]);
+                        self.emit(OpFalse, &[]);
                     }
                 }
                 Literal::String(s) => {
@@ -149,24 +157,24 @@ impl Compiler {
                     for element in array.elements.iter() {
                         self.compile_expr(element)?;
                     }
-                    self.emit(OpArray, &vec![array.elements.len()]);
+                    self.emit(OpArray, &[array.elements.len()]);
                 }
                 Literal::Hash(hash) => {
                     for (key, value) in hash.elements.iter() {
-                        self.compile_expr(&key)?;
-                        self.compile_expr(&value)?;
+                        self.compile_expr(key)?;
+                        self.compile_expr(value)?;
                     }
-                    self.emit(OpHash, &vec![hash.elements.len() * 2]);
+                    self.emit(OpHash, &[hash.elements.len() * 2]);
                 }
             },
             Expression::PREFIX(prefix) => {
-                self.compile_expr(&prefix.operand).unwrap();
+                self.compile_expr(&prefix.operand)?;
                 match prefix.op.kind {
                     TokenKind::MINUS => {
-                        self.emit(OpMinus, &vec![]);
+                        self.emit(OpMinus, &[]);
                     }
                     TokenKind::BANG => {
-                        self.emit(OpBang, &vec![]);
+                        self.emit(OpBang, &[]);
                     }
                     _ => {
                         return Err(format!("unexpected prefix op: {}", prefix.op));
@@ -175,90 +183,73 @@ impl Compiler {
             }
             Expression::INFIX(infix) => {
                 if infix.op.kind == TokenKind::LT {
-                    self.compile_expr(&infix.right).unwrap();
-                    self.compile_expr(&infix.left).unwrap();
-                    self.emit(Opcode::OpGreaterThan, &vec![]);
+                    self.compile_expr(&infix.right)?;
+                    self.compile_expr(&infix.left)?;
+                    self.emit(Opcode::OpGreaterThan, &[]);
                     return Ok(());
                 }
-                self.compile_expr(&infix.left).unwrap();
-                self.compile_expr(&infix.right).unwrap();
+                self.compile_expr(&infix.left)?;
+                self.compile_expr(&infix.right)?;
                 match infix.op.kind {
-                    TokenKind::PLUS => {
-                        self.emit(OpAdd, &vec![]);
-                    }
-                    TokenKind::MINUS => {
-                        self.emit(OpSub, &vec![]);
-                    }
-                    TokenKind::ASTERISK => {
-                        self.emit(OpMul, &vec![]);
-                    }
-                    TokenKind::SLASH => {
-                        self.emit(OpDiv, &vec![]);
-                    }
-                    TokenKind::GT => {
-                        self.emit(Opcode::OpGreaterThan, &vec![]);
-                    }
-                    TokenKind::EQ => {
-                        self.emit(Opcode::OpEqual, &vec![]);
-                    }
-                    TokenKind::NotEq => {
-                        self.emit(Opcode::OpNotEqual, &vec![]);
-                    }
-                    _ => {
-                        return Err(format!("unexpected infix op: {}", infix.op));
-                    }
+                    TokenKind::PLUS => self.emit(OpAdd, &[]),
+                    TokenKind::MINUS => self.emit(OpSub, &[]),
+                    TokenKind::ASTERISK => self.emit(OpMul, &[]),
+                    TokenKind::SLASH => self.emit(OpDiv, &[]),
+                    TokenKind::GT => self.emit(Opcode::OpGreaterThan, &[]),
+                    TokenKind::EQ => self.emit(Opcode::OpEqual, &[]),
+                    TokenKind::NotEq => self.emit(Opcode::OpNotEqual, &[]),
+                    _ => return Err(format!("unexpected infix op: {}", infix.op)),
                 }
             }
             Expression::IF(if_node) => {
                 self.compile_expr(&if_node.condition)?;
-                let jump_not_truthy = self.emit(OpJumpNotTruthy, &vec![9527]);
+                let jump_not_truthy = self.emit(OpJumpNotTruthy, &[Self::PLACEHOLDER_ADDRESS]);
                 self.compile_block_statement(&if_node.consequent)?;
                 if self.last_instruction_is(OpPop) {
                     self.remove_last_pop();
                 }
 
-                let jump_pos = self.emit(OpJump, &vec![9527]);
-
+                let jump_pos = self.emit(OpJump, &[Self::PLACEHOLDER_ADDRESS]);
                 let after_consequence_location = self.current_instruction().data.len();
                 self.change_operand(jump_not_truthy, after_consequence_location);
 
-                if if_node.alternate.is_none() {
-                    self.emit(OpNull, &vec![]);
-                } else {
-                    self.compile_block_statement(&if_node.clone().alternate.unwrap())?;
+                if let Some(alternate) = &if_node.alternate {
+                    self.compile_block_statement(alternate)?;
                     if self.last_instruction_is(OpPop) {
                         self.remove_last_pop();
                     }
+                } else {
+                    self.emit(OpNull, &[]);
                 }
+
                 let after_alternative_location = self.current_instruction().data.len();
                 self.change_operand(jump_pos, after_alternative_location);
             }
             Expression::Index(index) => {
                 self.compile_expr(&index.object)?;
                 self.compile_expr(&index.index)?;
-                self.emit(OpIndex, &vec![]);
+                self.emit(OpIndex, &[]);
             }
             Expression::FUNCTION(f) => {
                 self.enter_scope();
-                // f.name
-                for param in f.params.iter() {
+                for param in &f.params {
                     self.symbol_table.define(param.name.clone());
                 }
                 self.compile_block_statement(&f.body)?;
                 if self.last_instruction_is(OpPop) {
                     self.replace_last_pop_with_return();
                 }
-                if !(self.last_instruction_is(OpReturnValue)) {
-                    self.emit(OpReturn, &vec![]);
+                if !self.last_instruction_is(OpReturnValue) {
+                    self.emit(OpReturn, &[]);
                 }
                 let num_locals = self.symbol_table.num_definitions;
                 let free_symbols = self.symbol_table.free_symbols.clone();
                 let instructions = self.leave_scope();
-                for x in free_symbols.clone() {
-                    self.load_symbol(&x);
+                for symbol in &free_symbols {
+                    self.load_symbol(symbol);
                 }
 
-                let compiled_function = Rc::from(object::CompiledFunction {
+                let compiled_function = Rc::new(object::CompiledFunction {
                     instructions: instructions.data,
                     num_locals,
                     num_parameters: f.params.len(),
@@ -272,54 +263,43 @@ impl Compiler {
             }
             Expression::FunctionCall(fc) => {
                 self.compile_expr(&fc.callee)?;
-                for arg in fc.arguments.iter() {
+                for arg in &fc.arguments {
                     self.compile_expr(arg)?;
                 }
-                self.emit(OpCall, &vec![fc.arguments.len()]);
+                self.emit(OpCall, &[fc.arguments.len()]);
             }
         }
 
-        return Ok(());
+        Ok(())
     }
 
     fn load_symbol(&mut self, symbol: &Rc<Symbol>) {
         match symbol.scope {
-            SymbolScope::Global => {
-                self.emit(OpGetGlobal, &vec![symbol.index]);
-            }
-            SymbolScope::LOCAL => {
-                self.emit(OpGetLocal, &vec![symbol.index]);
-            }
-            SymbolScope::Builtin => {
-                self.emit(OpGetBuiltin, &vec![symbol.index]);
-            }
-            SymbolScope::Free => {
-                self.emit(OpGetFree, &vec![symbol.index]);
-            }
-            SymbolScope::Function => {
-                self.emit(OpCurrentClosure, &vec![]);
-            }
+            SymbolScope::Global => self.emit(OpGetGlobal, &[symbol.index]),
+            SymbolScope::LOCAL => self.emit(OpGetLocal, &[symbol.index]),
+            SymbolScope::Builtin => self.emit(OpGetBuiltin, &[symbol.index]),
+            SymbolScope::Free => self.emit(OpGetFree, &[symbol.index]),
+            SymbolScope::Function => self.emit(OpCurrentClosure, &[]),
         }
     }
 
     pub fn bytecode(&self) -> Bytecode {
-        return Bytecode {
+        Bytecode {
             instructions: self.current_instruction().clone(),
             constants: self.constants.clone(),
-        };
+        }
     }
 
     pub fn add_constant(&mut self, obj: Object) -> usize {
         self.constants.push(Rc::new(obj));
-        return self.constants.len() - 1;
+        self.constants.len() - 1
     }
 
-    pub fn emit(&mut self, op: Opcode, operands: &Vec<usize>) -> usize {
+    pub fn emit(&mut self, op: Opcode, operands: &[usize]) -> usize {
         let ins = make_instructions(op, operands);
         let pos = self.add_instructions(&ins);
         self.set_last_instruction(op, pos);
-
-        return pos;
+        pos
     }
 
     fn compile_block_statement(
@@ -334,11 +314,11 @@ impl Compiler {
 
     pub fn add_instructions(&mut self, ins: &Instructions) -> usize {
         let pos = self.current_instruction().data.len();
-        let updated_ins = self.scopes[self.scope_index]
+        self.scopes[self.scope_index]
             .instructions
-            .merge_instructions(ins);
-        self.scopes[self.scope_index].instructions = updated_ins;
-        return pos;
+            .data
+            .extend_from_slice(&ins.data);
+        pos
     }
 
     fn set_last_instruction(&mut self, op: Opcode, pos: usize) {
@@ -352,59 +332,49 @@ impl Compiler {
     }
 
     fn last_instruction_is(&self, op: Opcode) -> bool {
-        if self.current_instruction().data.len() == 0 {
+        if self.current_instruction().data.is_empty() {
             return false;
         }
-        return self.scopes[self.scope_index].last_instruction.opcode == op;
+        self.scopes[self.scope_index].last_instruction.opcode == op
     }
 
     fn remove_last_pop(&mut self) {
         let last = self.scopes[self.scope_index].last_instruction.clone();
         let previous = self.scopes[self.scope_index].previous_instruction.clone();
 
-        let old = self.current_instruction().data.clone();
-        let new = old[..last.position].to_vec();
-
-        self.scopes[self.scope_index].instructions.data = new;
+        self.scopes[self.scope_index]
+            .instructions
+            .data
+            .truncate(last.position);
         self.scopes[self.scope_index].last_instruction = previous;
     }
 
     fn replace_instruction(&mut self, pos: usize, new_instruction: &Instructions) {
-        let ins = &mut self.scopes[self.scope_index].instructions;
-        for i in 0..new_instruction.data.len() {
-            ins.data[pos + i] = new_instruction.data[i];
-        }
+        let ins = &mut self.scopes[self.scope_index].instructions.data;
+        ins.splice(
+            pos..pos + new_instruction.data.len(),
+            new_instruction.data.iter().cloned(),
+        );
     }
 
     fn replace_last_pop_with_return(&mut self) {
         let last_pos = self.scopes[self.scope_index].last_instruction.position;
-        self.replace_instruction(last_pos, &make_instructions(OpReturnValue, &vec![]));
+        self.replace_instruction(last_pos, &make_instructions(OpReturnValue, &[]));
         self.scopes[self.scope_index].last_instruction.opcode = OpReturnValue;
     }
 
     fn change_operand(&mut self, pos: usize, operand: usize) {
         let op = cast_u8_to_opcode(self.current_instruction().data[pos]);
-        let ins = make_instructions(op, &vec![operand]);
+        let ins = make_instructions(op, &[operand]);
         self.replace_instruction(pos, &ins);
     }
 
     fn current_instruction(&self) -> &Instructions {
-        return &self.scopes[self.scope_index].instructions;
+        &self.scopes[self.scope_index].instructions
     }
 
     fn enter_scope(&mut self) {
-        let scope = CompilationScope {
-            instructions: Instructions { data: vec![] },
-            last_instruction: EmittedInstruction {
-                opcode: OpNull,
-                position: 0,
-            },
-            previous_instruction: EmittedInstruction {
-                opcode: OpNull,
-                position: 0,
-            },
-        };
-        self.scopes.push(scope);
+        self.scopes.push(CompilationScope::default());
         self.scope_index += 1;
         self.symbol_table = SymbolTable::new_enclosed_symbol_table(self.symbol_table.clone());
     }
@@ -413,8 +383,7 @@ impl Compiler {
         let instructions = self.current_instruction().clone();
         self.scopes.pop();
         self.scope_index -= 1;
-        let s = self.symbol_table.outer.as_ref().unwrap().as_ref().clone();
-        self.symbol_table = s;
-        return instructions;
+        self.symbol_table = self.symbol_table.outer.as_ref().unwrap().as_ref().clone();
+        instructions
     }
 }
