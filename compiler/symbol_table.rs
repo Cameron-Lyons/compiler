4ee663e08e0d0006a5b62fc44136370168c7616d
Cameron Lyons <cameron.lyons2@gmail.com>
2025-01-28 14:49:35 -0600

    symbol table

--- compiler/symbol_table.rs
+++ compiler/symbol_table.rs
@@ -3,7 +3,7 @@ use std::rc::Rc;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum SymbolScope {
-    LOCAL,
+    Local,
     Global,
     Builtin,
     Free,
@@ -17,79 +17,105 @@ pub struct Symbol {
     pub index: usize,
 }
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Debug)]
 pub struct SymbolTable {
-    pub outer: Option<Rc<SymbolTable>>,
+    outer: Option<Rc<SymbolTable>>,
     symbols: HashMap<String, Rc<Symbol>>,
-    pub free_symbols: Vec<Rc<Symbol>>,
-    pub num_definitions: usize,
+    free_symbols: Vec<Rc<Symbol>>,
+    num_definitions: usize,
 }
 
 impl SymbolTable {
-    pub fn new() -> SymbolTable {
-        SymbolTable {
+    pub fn new() -> Self {
+        Self {
             symbols: HashMap::new(),
-            free_symbols: vec![],
+            free_symbols: Vec::new(),
             num_definitions: 0,
             outer: None,
         }
     }
 
-    pub fn new_enclosed_symbol_table(outer: SymbolTable) -> SymbolTable {
-        SymbolTable {
+    pub fn new_enclosed(outer: Rc<Self>) -> Self {
+        Self {
             symbols: HashMap::new(),
-            free_symbols: vec![],
+            free_symbols: Vec::new(),
             num_definitions: 0,
-            outer: Some(Rc::new(outer)),
+            outer: Some(outer),
         }
     }
 
-    pub fn define(&mut self, name: String) -> Rc<Symbol> {
-        let mut scope = SymbolScope::LOCAL;
-        if self.outer.is_none() {
-            scope = SymbolScope::Global;
-        }
+    pub fn define(&mut self, name: &str) -> Rc<Symbol> {
+        let scope = if self.outer.is_some() {
+            SymbolScope::Local
+        } else {
+            SymbolScope::Global
+        };
 
+        let name = name.to_string();
         let symbol = Rc::new(Symbol {
             name: name.clone(),
             index: self.num_definitions,
             scope,
         });
 
+        self.symbols.insert(name, Rc::clone(&symbol));
         self.num_definitions += 1;
-        self.symbols.insert(name.clone(), Rc::clone(&symbol));
-        return symbol;
+        symbol
     }
 
-    pub fn resolve(&self, name: String) -> Option<Rc<Symbol>> {
-        let symbol = self.symbols.get(&name);
-        if symbol.is_none() && self.outer.is_some() {
-            return self.outer.as_ref().unwrap().resolve(name);
+    pub fn resolve(&self, name: &str) -> Option<Rc<Symbol>> {
+        if let Some(symbol) = self.symbols.get(name) {
+            return Some(Rc::clone(symbol));
         }
-        return symbol.cloned();
+
+        if let Some(outer) = &self.outer {
+            let outer_symbol = outer.resolve(name);
+
+            if let Some(symbol) = outer_symbol {
+                match symbol.scope {
+                    SymbolScope::Local | SymbolScope::Free => {
+                        return Some(self.define_free_checked(symbol));
+                    }
+                    _ => return Some(symbol),
+                }
+            }
+        }
+
+        None
     }
 
-    pub fn define_builtin(&mut self, index: usize, name: String) -> Rc<Symbol> {
+    pub fn define_builtin(&mut self, index: usize, name: &str) -> Rc<Symbol> {
+        let name = name.to_string();
         let symbol = Rc::new(Symbol {
             name: name.clone(),
             index,
             scope: SymbolScope::Builtin,
         });
-        self.symbols.insert(name.clone(), Rc::clone(&symbol));
-        return symbol;
+        self.symbols.insert(name, Rc::clone(&symbol));
+        symbol
     }
 
-    pub fn define_function_name(&mut self, name: String) -> Rc<Symbol> {
+    pub fn define_function_name(&mut self, name: &str) -> Rc<Symbol> {
+        let name = name.to_string();
         let symbol = Rc::new(Symbol {
             name: name.clone(),
             index: 0,
             scope: SymbolScope::Function,
         });
-        self.symbols.insert(name.clone(), Rc::clone(&symbol));
-        return symbol;
+        self.symbols.insert(name, Rc::clone(&symbol));
+        symbol
     }
 
-    pub fn define_free(&mut self, original: Rc<Symbol>) -> Rc<Symbol> {
+    fn define_free_checked(&self, original: Rc<Symbol>) -> Rc<Symbol> {
+        if let Some(existing) = self.symbols.get(&original.name) {
+            return Rc::clone(existing);
+        }
+
+        let mut self_mut = unsafe { &mut *(self as *const Self as *mut Self) };
+        self_mut.define_free(original)
+    }
+
+    fn define_free(&mut self, original: Rc<Symbol>) -> Rc<Symbol> {
         self.free_symbols.push(Rc::clone(&original));
         let symbol = Rc::new(Symbol {
             name: original.name.clone(),
@@ -98,6 +124,19 @@ impl SymbolTable {
         });
         self.symbols
             .insert(original.name.clone(), Rc::clone(&symbol));
-        return symbol;
+        symbol
+    }
+
+    // Accessor methods
+    pub fn num_definitions(&self) -> usize {
+        self.num_definitions
+    }
+
+    pub fn free_symbols(&self) -> &[Rc<Symbol>] {
+        &self.free_symbols
+    }
+
+    pub fn outer(&self) -> Option<&Rc<Self>> {
+        self.outer.as_ref()
     }
 }
